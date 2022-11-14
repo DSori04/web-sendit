7c27a2230df774ba891ca6e5df21f9d58e600f4a
AagieKLZ <darkyelan@gmail.com>
2022-11-14 21:25:24 +0100

    Added headers and footers

--- src/LogIn/LogIn.jsx
+++ src/LogIn/LogIn.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Navbar } from "../SharedComponents/Navbar";
+import { Footer } from "../SharedComponents/Footer";
 
 export function LogIn() {
     return (
+        <>
+        <Navbar/>
         <div>
         <h1>Log In</h1>
         </div>
+        <Footer/>
+        </>
     );
 }
