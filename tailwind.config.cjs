26576faf20b021ec93131995646e1345b2705828
AagieKLZ <darkyelan@gmail.com>
2022-11-14 17:19:45 +0100

    Tailwind config updated - Added theming

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,22 @@
 module.exports = {
   content: [
     './index.html',
-    "./src/**/*.{tsx,ts}",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
   ],
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'gray1' : '#EDEDED',
+      'purple1' : '#7903C1',
+      'purple2' : '#E4B8FF',
+      'red1' : '#FF0000',
+      'gray2' : '#4A4A4A'
+    },
+    fontFamily: {
+      'main' : 'Montserrat',
+      'numbers' : 'IBM Plex Mono'
+    },
     extend: {},
   },
   plugins: [],
