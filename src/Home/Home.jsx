feea5e880523d2421f2806d04f7d6cecb1e7940e
AagieKLZ <darkyelan@gmail.com>
2022-11-15 11:44:01 +0100

    Center home text responsive - TODO: Check sm size

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,12 +9,12 @@ export function Home() {
         <>
             <Navbar />
             <div className="absolute top-16 sm:bottom-14 bottom-28 bg-white w-full h-9/12 flex flex-col">
-                <div id="text-main" className="flex flex-row w-9/12 justify-around ml-48 mt-12 flex-wrap-reverse">
+                <div id="text-main" className="flex flex-row w-9/12 justify-around sm:ml-32 lg:mt-12 mt-4 flex-wrap-reverse">
                     <div id="text-content" className="flex flex-col w-5/12 mt-32">
-                        <div id="text" className="w-full h-1/2 tracking-wider font-bold text-right text-4xl font-main">Nunca hemos <br></br> estado tan <span className="text-purple1">cerca</span></div>
+                        <div id="text" className="w-full h-1/2 tracking-wider font-bold lg:text-right text-center lg:text-4xl text-3xl font-main">Nunca hemos <br></br> estado tan <span className="text-purple1">cerca</span></div>
                         <div id="conocenos" className="w-full mt-5 flex flex-row sm:justify-around justify-center">
-                            <div className="w-1/2 flex-start"></div>
-                            <div className="flex-end w-40 font-main mt-4 lg:m-0 drop-shadow-lg border-purple1 hover:scale-105 font-semibold text-xl py-2 border-2 text-white bg-purple1 rounded-full text-center cursor-pointer">
+                            <div className="w-1/2 flex-start lg:static hidden"></div>
+                            <div className="flex-end w-max px-2 font-main mt-4 lg:m-0 drop-shadow-lg border-purple1 hover:scale-105 font-semibold text-xl py-2 border-2 text-white bg-purple1 rounded-full text-center cursor-pointer">
                                 <Link to="/about-us">¡Conócenos!</Link>
                             </div>
                         </div>
