9feecca5abfdfeb246eca3ecab2be16f921e0e38
AagieKLZ <darkyelan@gmail.com>
2022-11-15 10:12:19 +0100

    Shadow button

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -12,10 +12,10 @@ export function Home() {
                 <div id="text-main" className="flex flex-row w-9/12 justify-around ml-48 mt-12 flex-wrap-reverse">
                     <div id="text-content" className="flex flex-col w-5/12 mt-32">
                         <div id="text" className="w-full h-1/2 tracking-wider font-bold text-right text-4xl font-main">Nunca hemos <br></br> estado tan <span className="text-purple1">cerca</span></div>
-                        <div id="conocenos" className="w-full mt-5 flex flex-row justify-around">
+                        <div id="conocenos" className="w-full mt-5 flex flex-row sm:justify-around justify-center">
                             <div className="w-1/2 flex-start"></div>
-                            <div className="flex-end w-40 font-main mt-4 lg:m-0 font-semibold text-xl py-2 border-2 text-white bg-purple1 rounded-full text-center cursor-pointer">
-                                <Link href="/about-us">¡Conócenos!</Link>
+                            <div className="flex-end w-40 font-main mt-4 lg:m-0 drop-shadow-lg border-purple1 hover:scale-105 font-semibold text-xl py-2 border-2 text-white bg-purple1 rounded-full text-center cursor-pointer">
+                                <Link to="/about-us">¡Conócenos!</Link>
                             </div>
                         </div>
                     </div>
