5d05cd45b26f538d896715d602c18338b7980d7a
AagieKLZ <darkyelan@gmail.com>
2022-11-15 12:10:54 +0100

    Fixed button position

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -13,7 +13,7 @@ export function Home() {
                     <div id="text-content" className="flex flex-col w-5/12 mt-32">
                         <div id="text" className="w-full h-1/2 lg:tracking-wider tracking-wide font-bold lg:text-right text-center lg:text-4xl sm:text-3xl text-2xl font-main">Nunca hemos <br></br> estado tan <span className="text-purple1">cerca</span></div>
                         <div id="conocenos" className="w-full mt-5 flex flex-row sm:justify-around justify-center">
-                            <div className="w-1/2 flex-start lg:static hidden"></div>
+                            <div className="w-1/2 flex-start lg:static"></div>
                             <div className="flex-end w-max px-2 font-main mt-4 lg:m-0 drop-shadow-lg border-purple1 hover:scale-105 font-semibold text-xl py-2 border-2 text-white bg-purple1 rounded-full text-center cursor-pointer">
                                 <Link to="/about-us">¡Conócenos!</Link>
                             </div>
