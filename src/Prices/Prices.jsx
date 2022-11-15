c8f012e034d04e680c3c25836f73e9752b0fcab6
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-11-15 12:28:31 +0100

    prices terminado

--- src/Prices/Prices.jsx
+++ src/Prices/Prices.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "../SharedComponents/Navbar";
 import { Footer } from "../SharedComponents/Footer";
+import { Link } from "react-router-dom";
 import savings from "./assets/savings.svg";
 
 
@@ -38,9 +39,9 @@ export function Prices() {
         <>
             <Navbar />
             <div className="absolute top-16 sm:bottom-14 bottom-28 bg-white w-full  flex flex-col md:px-20 px-6">
-                <h1 className=" font-bold text-purple1 text-5xl pt-10">Precios</h1>
+                <h1 className=" font-bold text-purple1 text-5xl lg:pt-14 pt-8">Precios</h1>
                 <p className=" py-10 text-2xl">
-                    Con nosotros, estar cerca vale <span className="font-bold ">menos que nunca</span>
+                    Con nosotros, estar cerca vale <span className="font-bold text-purple1">menos que nunca</span>
                 </p>
                 <div className="flex flex-row w-full">
                     <div className="flex flex-col lg:w-1/2 w-full">
@@ -61,16 +62,23 @@ export function Prices() {
                                 )
                             })}
                         </table>
+                        <div className=" lg:self-start self-center">
+                            <div id="makeOrder" className="bg-purple1 cursor-pointer text-white rounded-full w-52 h-14 mt-14 text-center drop-shadow-lg hover:scale-105 text-xl">
+                                <span className=" leading-[3.5rem] align-middle">
+                                    Nuevo pedido
+                                </span>
+                            </div>
+                        </div>
+                        <div className="py-8 lg:text-left text-center w-full font-extralight">
+                                ¿Eres una pequeña empresa?
+                                <Link to="/contact" className=" text-purple1 underline"> ¡Utiliza nuestros beneficios!</Link>
+                            </div>
                     </div>
                     <div className="flex-col lg:flex hidden lg:w-1/2 h-full`">
                         <img src={savings} alt="Imagen de ahorros" className=" max-h-72 mt-20" />
                     </div>
                 </div>
-                <div id="makeOrder" className="bg-purple1 cursor-pointer align-middle text-white rounded-full w-52 h-14 mt-14 text-center drop-shadow-lg hover:scale-105 text-xl font-light">
-                    <span className=" leading-[3.5rem] align-middle">
-                            Nuevo pedido
-                    </span>
-                </div>
+
             </div>
             <Footer />
         </>
