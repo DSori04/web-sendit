e3150ff5a97d82c539fb00edfdb957ea9afcdf5e
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-11-15 10:32:10 +0100

    added hover effects navbar & footer

--- src/SharedComponents/Footer.jsx
+++ src/SharedComponents/Footer.jsx
@@ -9,18 +9,26 @@ export function Footer() {
     return (
         <div className="flex h-full">
             <div className="flex-end bg-gray1 w-full sm:h-14 h-28 flex justify-around fixed bottom-0 self-center flex-wrap">
-            <div className="flex text-center h-14 leading-[3.5rem] align-middle pt-3 w-40 justify-around">
-                {/* TODO Añadir iconitos para cuando el tamaño sea inferior a lg */}
-                <a href="https://twitter.com" target="_blank"><img src={Twitter} className="fill-black w-8 h-8"></img></a>
-                <a href="https://facebook.com" target="_blank"><img src={Facebook} className="fill-black w-8 h-8"></img></a>
-                <a href="https://instagram.com" target="_blank"><img src={Instagram} className="fill-black w-8 h-8"></img></a>
-                <a href="https://github.com" target="_blank"><img src={Github} className="fill-black w-8 h-8"></img></a>
+                <div className="flex text-center h-14 leading-[3.5rem] align-middle pt-3 w-40 justify-around">
+                    {/* TODO Añadir iconitos para cuando el tamaño sea inferior a lg */}
+                    <a href="https://twitter.com" target="_blank"><img src={Twitter} className="fill-black w-8 h-8"></img></a>
+                    <a href="https://facebook.com" target="_blank"><img src={Facebook} className="fill-black w-8 h-8"></img></a>
+                    <a href="https://instagram.com" target="_blank"><img src={Instagram} className="fill-black w-8 h-8"></img></a>
+                    <a href="https://github.com" target="_blank"><img src={Github} className="fill-black w-8 h-8"></img></a>
+                </div>
+                <div className=" lg:self-auto sm:w-auto text-center leading-[3.5rem] align-middle h-14 w-full">
+                    <Link to="/about-us">
+                        <span className=" hover:underline">About Us</span> &nbsp;
+                    </Link>
+                    · &nbsp;
+                    <span className=" hover:underline">LOPD</span> &nbsp;
+                    · &nbsp;
+                    <Link to="/contact">
+                        <span className=" hover:underline">Contacto</span>
+                    </Link></div>
             </div>
-            <div className=" lg:self-auto sm:w-auto text-center leading-[3.5rem] align-middle h-14 w-full">
-                <Link to="/about-us">About Us</Link> · LOPD · <Link to="/contact">Contacto</Link></div>
-            </div>
-            
+
         </div>
-        
+
     );
 }
--- src/SharedComponents/Navbar.jsx
+++ src/SharedComponents/Navbar.jsx
@@ -12,9 +12,9 @@ export function Navbar() {
             <div id="menu" className="lg:flex hidden flex-row w-9/12 text-center align-middle justify-around pl-16 select-none font-main text-gray2">
                 <div className="flex flex-col justify-around w-1/2">
                 <div id="imgs" className="flex flex-row justify-between">
-                    <Link to="/tracking"><img src={Tracking} className="text-center w-16 h-12 pt-2 pl-2 pb-1" alt="tracking-icon"></img></Link>
-                    <Link to="/new"><img src={Box} className="text-center h-10 pt-2 pl-2" alt="box-icon"></img></Link>
-                    <Link to="/prices"><img src={Prices} className="text-center h-12 w-16" alt="prices-icon"></img></Link>
+                    <Link to="/tracking"><img src={Tracking} className="text-center w-16 h-12 pt-2 pl-2 pb-1 hover:scale-110" alt="tracking-icon"></img></Link>
+                    <Link to="/new"><img src={Box} className="text-center h-10 pt-2 pl-2 hover:scale-110" alt="box-icon"></img></Link>
+                    <Link to="/prices"><img src={Prices} className="text-center h-12 w-16 hover:scale-110" alt="prices-icon"></img></Link>
                 </div>
                 <div id="links" className="flex flex-row justify-between -mt-2">
                     <Link to="/tracking" className="hover:underline text-center ">Tracking</Link>
@@ -24,7 +24,7 @@ export function Navbar() {
                 </div>
                 <div className="flex justify-around w-96">
                     <div id="login" className="ml-28 cursor-pointer align-middle leading-[3.5rem] h-16"><p className="text-purple1 font-main font-semibold pt-1">Log In</p></div>
-                    <div id="signup" className="bg-purple1 cursor-pointer align-middle leading-[2rem] text-white rounded-full mt-4 w-28 h-8 text-center -ml-8 font-semibold drop-shadow-lg hover:scale-105 ">Sign Up</div>
+                    <div id="signup" className="bg-purple1 cursor-pointer align-middle leading-[2rem] text-white rounded-full mt-4 w-28 h-8 text-center -ml-8 font-semibold drop-shadow-lg hover:scale-105">Sign Up</div>
                 </div>
                 
             </div>
