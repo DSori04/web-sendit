f7b470f910886d9f6ee8b7eeaa96bf530889962c
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-11-15 13:54:20 +0100

    Home responsive

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -12,10 +12,10 @@ export function Home() {
     return (
         <>
             <Navbar />
-            <div className="absolute top-16 bg-white w-full h-max flex flex-col">
-                <div id="text-main" className="flex flex-row w-9/12 justify-around sm:ml-32 lg:mt-12 mt-4 flex-wrap-reverse">
-                    <div id="text-content" className="flex flex-col w-5/12 mt-32">
-                        <div id="text" className="flex flex-col w-full h-max lg:tracking-wider tracking-wide font-bold lg:text-right text-center xl:test-5xl lg:text-4xl sm:text-3xl text-2xl font-main">
+            <div className=" pt-16 bg-white w-full h-max flex flex-col">
+                <div id="text-main" className="flex flex-row justify-around mt-12 flex-wrap-reverse w-full">
+                    <div id="text-content" className="flex flex-col lg:w-4/12 w-full lg:mt-32 mt-10">
+                        <div id="text" className="flex flex-col w-full h-max lg:tracking-wider tracking-wide font-bold lg:text-right text-center xl:test-9xl lg:text-4xl sm:text-3xl text-2xl font-main">
                             Nunca hemos <br></br> estado tan <span className="text-purple1">cerca</span>
                             <div className="float w-full mt-4">
                                 <div className=" grid lg:place-content-end place-content-center">
@@ -27,9 +27,10 @@ export function Home() {
 
                         </div>
                     </div>
-                    <div id="home-img"><img src={HomeImg}></img></div>
-                </div>
-                <div id="counters" className="w-full h-48 flex justify-around text-center sm:mt-32 mt-0 lg:scale-100 scale-75">
+                    <div id="home-img" className="lg:-ml-44"><img src={HomeImg}></img></div>
+                    
+                </div> <br />  
+                <div id="counters" className="w-full h-48 flex justify-around text-center xl:mt-48 lg:mt-36 mt-12 lg:scale-100 scale-75">
                     <div id="clientes" className="flex flex-col">
                         <div className="text-7xl font-numbers font-semibold">{clients}k</div>
                         <div className="font-main text-xl">Clientes satisfechos</div>
