f345c9b9f7d983aa7ac9e5aa5c8742d66daec8c5
AagieKLZ <darkyelan@gmail.com>
2022-11-15 09:30:50 +0100

    FIXED: Footer doesnt overlap anymore

--- src/SharedComponents/Footer.jsx
+++ src/SharedComponents/Footer.jsx
@@ -7,7 +7,8 @@ import { Link } from "react-router-dom";
 
 export function Footer() {
     return (
-        <div className="bg-gray1 w-full sm:h-14 h-28 flex justify-around absolute bottom-0 self-center flex-wrap">
+        <div className="relative h-screen">
+            <div className="bg-gray1 w-full sm:h-14 h-28 flex justify-around absolute bottom-0 self-center flex-wrap">
             <div className="flex text-center h-14 leading-[3.5rem] align-middle pt-3 w-40 justify-around">
                 {/* TODO Añadir iconitos para cuando el tamaño sea inferior a lg */}
                 <a href="https://twitter.com" target="_blank"><img src={Twitter} className="fill-black w-8 h-8"></img></a>
@@ -18,5 +19,7 @@ export function Footer() {
             <div className=" lg:self-auto sm:w-auto text-center leading-[3.5rem] align-middle h-14 w-full">
                 <Link to="/about-us">About Us</Link> · LOPD · <Link to="/contact">Contacto</Link></div>
         </div>
+        </div>
+        
     );
 }
