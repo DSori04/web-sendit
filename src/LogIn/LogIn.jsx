4e4c4cb6d0cbd3c45a27dc94bc8ae0f9e000688b
AagieKLZ <darkyelan@gmail.com>
2022-11-16 13:31:06 +0100

    Work on Log In / Sign Up

--- src/LogIn/LogIn.jsx
+++ src/LogIn/LogIn.jsx
@@ -22,9 +22,9 @@ export function LogIn() {
                         <div className="flex flex-col relative mt-12 lg:ml-24">
                             <form>
                                 <label htmlFor="mail" className="text-main block mt-8">Correo Electrónico</label>
-                                <input type="text" className="border-b-2 block"></input>
+                                <input type="email" className="border-b-2 block" required onInvalid={(e) => e.target.setCustomValidity('Introduce un correo electrónico válido')} onInput={(e) => e.target.setCustomValidity('')}></input>
                                 <label htmlFor="pw" className="text-main block mt-8">Contraseña</label>
-                                <input type="password" className="border-b-2"></input>
+                                <input type="password" className="border-b-2" required onInvalid={(e) => e.target.setCustomValidity('Introduce una contraseña')} onInput={(e) => e.target.setCustomValidity('')}></input>
                                 <input type="submit" value="Log In" className="block mt-8 bg-purple1 font-main text-white px-4 py-1 rounded-full font-semibold drop-shadow-xl lg:hover:hue-rotate-15"/>
                             </form>
                         </div>
--- src/LogIn/SignUp.jsx
+++ src/LogIn/SignUp.jsx
@@ -4,12 +4,41 @@ import { Footer } from "../SharedComponents/Footer";
 import { useState, useEffect } from "react"
 import LoginIcon from "./assets/undraw_world_re_768.svg"
 import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
 
 export function SignUp(){
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [mail, setMail] = useState("");
+    const [pw, setPw] = useState("");
+    const [pwrepeat, setPwrepeat] = useState("");
+    const [accepted, setAccepted] = useState(false);
+
+    function checkPw(e){
+        const val = e.target.value
+        console.log(val.length, e.target.minLength);
+        if (val.length < e.target.minLength){
+            e.target.setCustomValidity("La contraseña es demasiado corta")
+        } else{
+            e.target.setCustomValidity("Este campo es obligatoriooo")
+        }
+    }
+
+    function repeatingPw(e){
+        console.log(e.target.value);
+        if (e.target.value == ""){
+            e.target.setCustomValidity("Este campo es obligatorio")
+        } else if (e.target.value != "" && e.target.value != pw){
+            e.target.setCustomValidity("Las contraseñas no coinciden")
+        } else{
+            setPwrepeat(e.target.value)
+        }
+    }
+
     return (
         <>
             <Navbar />
-            <div className="lg:absolute lg:pt-20 pt-16 top-16 sm:bottom-14 bottom-28 w-full flex flex-col md:px-20 px-6 font-main lg:min-h-0 min-h-screen">
+            <div className="lg:absolute lg:pt-20 pt-16 top-16 sm:bottom-14 bottom-28 w-full flex flex-col md:px-20 px-6 font-main lg:min-h-0 min-h-screen select-none">
                 <div className="flex lg:flex-row flex-col w-full">
                     <div className="flex flex-col lg:w-1/2 w-full h-auto ml-24">
                         <div className="flex flex-row justify-start pt-3 font-main -ml-4">
@@ -21,16 +50,24 @@ export function SignUp(){
                         </div>
                         <div className="flex flex-col relative mt-12 lg:ml-24">
                             <form>
-                                <label htmlFor="name" className="text-main block mt-8">Nombre</label>
-                                <input type="text" className="border-b-2 block"></input>
-                                <label htmlFor="name" className="text-main block mt-8">Apellidos</label>
-                                <input type="text" className="border-b-2 block"></input>
-                                <label htmlFor="name" className="text-main block mt-8">Correo Electrónico</label>
-                                <input type="email" className="border-b-2 block"></input>
-                                <label htmlFor="pw" className="text-main block mt-8">Contraseña</label>
-                                <input type="password" className="border-b-2"></input>
-                                <label htmlFor="pw" className="text-main block mt-8">Repetir Contraseña</label>
-                                <input type="password" className="border-b-2"></input>
+                                <label htmlFor="name" className="text-main block mt-8">Nombre <span className="text-main text-red1">*</span></label>
+                                <input type="text" name="name" className="border-b-2 block" required onChange={(e) => setName(e.target.value)} onInvalid={(e) => e.target.setCustomValidity('Este campo es obligatorio')} onInput={(e) => e.target.setCustomValidity('')}></input>
+                                
+                                <label htmlFor="surname" className="text-main block mt-8">Apellidos <span className="text-main text-red1">*</span></label>
+                                <input type="text" name="surname" className="border-b-2 block" required onChange={(e) => setSurname(e.target.value)} onInvalid={(e) => e.target.setCustomValidity('Este campo es obligatorio')} onInput={(e) => e.target.setCustomValidity('')}></input>
+                                
+                                <label htmlFor="email" className="text-main block mt-8">Correo Electrónico <span className="text-main text-red1">*</span></label>
+                                <input type="email" name="email" className="border-b-2 block" required onChange={(e) => setMail(e.target.value)} onInvalid={(e) => e.target.setCustomValidity('Introduce un correo electrónico válido')} onInput={(e) => e.target.setCustomValidity('')}></input>
+                                
+                                <label htmlFor="pw" className="text-main block mt-8">Contraseña <span className="text-main text-red1">*</span></label>
+                                <input type="password" name="pw" className="border-b-2" required onChange={(e) => setPw(e.target.value)} minLength={6} onInvalid={(e) => setPwrepeat(e)} onInput={(e) => e.target.setCustomValidity('')}></input>
+                                
+                                <label htmlFor="pwrepeat" className="text-main block mt-8">Repetir Contraseña <span className="text-main text-red1">*</span></label>
+                                <input type="password" name="pwrepeat" className="border-b-2" required minLength={6} onInvalid={(e) => checkPw(e)} onChange={(e) => setPwrepeat(e.target.value)} onInput={(e) => e.target.setCustomValidity('')}></input><br></br>
+                                
+                                <input type="checkbox" name="checkTerms" className="inline-block mt-8" required onInvalid={(e) => e.target.setCustomValidity('Este campo es obligatorio')} onInput={(e) => e.target.setCustomValidity('')}></input>                                
+                                <label htmlFor="checkTerms"> Acepto los <span className="text-purple1 font-main underline cursor-pointer">términos de servicio</span> <span className="text-main text-red1">*</span></label>
+
                                 <input type="submit" value="Sign Up" className="block mt-8 bg-purple1 font-main text-white px-4 py-1 rounded-full font-semibold drop-shadow-xl hover:hue-rotate-15"/>
                             </form>
                         </div>
