b7ef395feceda3ea8c099aac3ee209d317ef8efe
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-11-16 09:19:28 +0100

    MIAU

--- src/NotFound/NotFound.jsx
+++ src/NotFound/NotFound.jsx
@@ -8,29 +8,29 @@ export function NotFound() {
     console.log("NotFound");
 
     return (
-        <div className="h-screen max-h-screen">
+        <>
+            <div className="h-screen max-h-screen">
+                <Navbar />
 
-            <Navbar />
+                <div className="w-full bg-white flex justify-center items-center p-10 lg:pt-32">
+                    <img src={imagen404} alt="404" className=" max-h-96 lg:pt-0 pt-32 " />
+                </div>
+                <div className="flex justify-center items-center text-center lg:pt-7 pt-10 ml-8 mr-8">
+                    <h1 className="lg:text-3xl text-4xl leading-[4rem]">
+                        <span className=" font-bold">
+                            ¡Woops!
+                        </span>
+                        &nbsp; esta página no existe - &nbsp;
+                        <span className=" lg:hidden inline">
+                            <br />
+                        </span>
+                        <a href="/" className=" text-purple1 underline hover:hue-rotate-15">Volver a inicio</a>
 
-            <div className="w-full bg-white flex justify-center items-center p-10 lg:pt-32">
-                <img src={imagen404} alt="404" className=" max-h-96 lg:pt-0 pt-32 " />
+                    </h1>
+                </div>
+
+                <Footer />
             </div>
-            <div className="flex justify-center items-center text-center lg:pt-7 pt-10 ml-8 mr-8">
-                <h1 className="lg:text-3xl text-4xl leading-[4rem]">
-                    <span className=" font-bold">
-                        ¡Woops!
-                    </span>
-                    &nbsp; esta página no existe - &nbsp;
-                    <span className=" lg:hidden inline">
-                        <br />
-                    </span>
-                    <a href="/" className=" text-purple1 underline hover:hue-rotate-15">Volver a inicio</a>
-
-                </h1>
-            </div>
-
-            <Footer />
-
-        </div>
+        </>
     );
 }
