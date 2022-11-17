ffacea24cc3320f74f3b09d2de2b6b61d118fa03
AagieKLZ <darkyelan@gmail.com>
2022-11-17 16:24:56 +0100

    Navbar hover effects

--- src/SharedComponents/Navbar.jsx
+++ src/SharedComponents/Navbar.jsx
@@ -4,26 +4,32 @@ import Tracking from "./assets/Tracking.svg"
 import Box from "./assets/Box.svg"
 import Prices from "./assets/Prices.svg"
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 
 
 
 export function Navbar() {
+    const [tracking, setTracking] = useState(false)
+    const [newMenu, setNew] = useState(false);
+    const [price, setPrice] = useState(false)
+
     return (
         <nav className="bg-gray1 w-full h-16 flex lg:justify-between z-40 justify-center lg:pl-24 select-none fixed">
             <div id="logo" className="flex-start h-14 w-48"><Link to="/"><img src={Logo} className="object-contain"></img></Link></div>
             <div id="menu" className="lg:flex hidden flex-row w-9/12 text-center align-middle justify-around pl-16 select-none font-main text-gray2">
                 <div className="flex flex-col justify-around w-1/2">
-                <div id="imgs" className="flex flex-row justify-between">
-                    <Link to="/tracking"><img src={Tracking} className="text-center w-16 h-12 pt-2 pl-2 pb-1 hover:scale-110" alt="tracking-icon"></img></Link>
-                    <Link to="/new"><img src={Box} className="text-center h-10 pt-2 pl-2 hover:scale-110" alt="box-icon"></img></Link>
-                    <Link to="/prices"><img src={Prices} className="text-center h-12 w-16 hover:scale-110" alt="prices-icon"></img></Link>
+                <div id="imgs" className="flex flex-row justify-around"  >
+                    <Link to="/tracking" onMouseEnter={() => setTracking(true)} onMouseLeave={() => setTracking(false)}><img src={Tracking} className="text-center w-16 h-12 pt-2 pl-2 pb-1 hover:scale-110" alt="tracking-icon"></img></Link>
+                    <Link to="/new" onMouseEnter={() => setNew(true)} onMouseLeave={() => setNew(false)}><img src={Box} className="text-center h-10 pt-2 pl-2 hover:scale-110" alt="box-icon"></img></Link>
+                    <Link to="/prices" onMouseEnter={() => setPrice(true)} onMouseLeave={() => setPrice(false)}><img src={Prices} className="text-center h-12 w-16 hover:scale-110" alt="prices-icon"></img></Link>
                 </div>
-                <div id="links" className="flex flex-row justify-between -mt-2">
-                    <Link to="/tracking" className="hover:underline text-center ">Tracking</Link>
-                    <Link to="/new" className="hover:underline text-center">Nuevo</Link>
-                    <Link to="/prices" className="hover:underline text-center">Precios</Link>
+                <div id="links" className={`flex flex-row justify-around -mt-2`}>
+                    <Link to="/tracking" className={`text-center ${tracking ? 'text-gray2' : 'text-gray1'}`}>Tracking</Link>
+                    <Link to="/new" className={`text-center ${newMenu ? 'text-gray2' : 'text-gray1'}`}>Nuevo</Link>
+                    <Link to="/prices" className={`text-center ${price ? 'text-gray2' : 'text-gray1'}`}>Precios</Link>
                 </div>
+                
                 </div>
                 <div className="flex justify-around w-96">
                     <Link to="/login"><div id="login" className="ml-28 cursor-pointer align-middle leading-[3.5rem] h-16"><p className="text-purple1 font-main font-semibold pt-1 hover:hue-rotate-15">Log In</p></div></Link>
