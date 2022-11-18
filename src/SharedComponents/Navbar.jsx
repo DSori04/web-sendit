63f10a3d09da94562ba08444538ee2fa3a824de5
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-11-18 09:13:44 +0100

    Navbar PC (a medias)

--- src/SharedComponents/Navbar.jsx
+++ src/SharedComponents/Navbar.jsx
@@ -19,7 +19,7 @@ export function Navbar() {
             <nav className="bg-gray1 w-full h-16 flex lg:justify-between z-40 justify-between lg:pl-24 select-none fixed">
                 <div id="logo" className="flex-start h-14 w-48"><Link to="/"><img src={Logo} className="object-contain"></img></Link></div>
                 <div id="menu" className="lg:flex hidden flex-row w-9/12 text-center align-middle justify-around pl-16 select-none font-main text-gray2">
-                    <div className="flex flex-col justify-around w-1/2">
+                    <div className="flex lg:flex-row flex-col justify-end w-1/2">
                         <div id="imgs" className="flex flex-row justify-around"  >
                             <Link to="/tracking" onMouseEnter={() => setTracking(true)} onMouseLeave={() => setTracking(false)}>
                                 <img src={Tracking} className="text-center w-16 h-12 pt-2 pl-2 pb-1 hover:scale-110" alt="tracking-icon">
@@ -34,7 +34,7 @@ export function Navbar() {
                                 </img>
                             </Link>
                         </div>
-                        <div id="links" className={`flex flex-row justify-around -mt-2`}>
+                        <div id="links" className="flex flex-row justify-around -mt-2 lg:hidden">
                             <Link to="/tracking" className={`text-center ${tracking ? 'text-gray2' : 'text-gray1'}`}>
                                 Tracking
                             </Link>
@@ -48,8 +48,20 @@ export function Navbar() {
 
                     </div>
                     <div className="flex justify-around w-96">
-                        <Link to="/login"><div id="login" className="ml-28 cursor-pointer align-middle leading-[3.5rem] h-16"><p className="text-purple1 font-main font-semibold pt-1 hover:hue-rotate-15">Log In</p></div></Link>
-                        <Link to="/signup"><div id="signup" className="bg-purple1 cursor-pointer align-middle leading-[2rem] text-white rounded-full mt-4 w-28 h-8 text-center -ml-8 font-semibold drop-shadow-lg hover:scale-105 hover:hue-rotate-15">Sign Up</div></Link>
+                        <div className=" w-full flex flex-col justify-end">
+                            <Link to="/login">
+                                <div id="login" className=" cursor-pointer align-middle leading-[3.5rem] h-16">
+                                    <p className="text-purple1 font-main font-semibold pt-1 hover:hue-rotate-15">
+                                        Log In
+                                    </p>
+                                </div>
+                            </Link>
+                        </div>
+                        <Link to="/signup">
+                            <div id="signup" className="bg-purple1 cursor-pointer align-middle leading-[2rem] text-white rounded-full mt-4 w-28 h-8 text-center -ml-8 font-semibold drop-shadow-lg hover:scale-105 hover:hue-rotate-15">
+                                Sign Up
+                            </div>
+                        </Link>
                     </div>
                 </div>
                 <div className="lg:hidden flex pr-2 w-16" onClick={() => setVis(!sideMenu)}>
