0fbfb4766c141218ee5796b8763bf9382dc7e2d9
AagieKLZ <darkyelan@gmail.com>
2022-11-22 08:44:32 +0100

    Server Backend basics

---
+++ server.js
@@ -0,0 +1,88 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//For Login - Returns user data
+app.post('/user', (req, res) => {
+    res.send({Auth : 'success'})
+    //res.send({Auth : 'failed'})
+})
+
+//To create users
+app.put('/user', (req, res) => {
+    res.send({Success : true})
+    //res.send({Success: false})
+})
+
+//To update users
+app.put('/user/:user_id', (req, res) => {
+    res.send({Success : true})
+    //res.send({Success: false})
+})
+
+//To delete users
+app.delete('/user', (req, res) => {
+    res.send({Success: true})
+    //res.send({Success: false})
+})
+
+//To get Prices
+app.get('/prices', (req, res) => {
+    const prices = [
+        {'Tier' : 'S', 'minDistance' : 0, 'maxDistance' : 5, 'Price' : 1}
+    ]
+    res.send(prices)
+})
+
+//Update prices
+app.put('/prices', (req, res) => {
+    //
+})
+
+app.delete('/prices', (req, res) => {})
+
+//To add an address - Returns address id
+app.post('/address', (req, res) => {
+    res.send({Success : true})
+    //res.send({Success: false})
+})
+
+//To get an address
+app.get('/address', (req, res) => {
+    res.send({Success : true})
+    //res.send({Success: false})
+})
+
+//To add personal info - Returns info id
+app.post('/info', (req, res) => {
+    res.send({Success : true})
+    //res.send({Success: false})
+})
+
+//To get info 
+app.get('/info', (req, res) => {
+    res.send({Success : true})
+    //res.send({Success: false})
+})
+
+//To add orders
+app.put('/orders', (req, res) => {
+    res.send()
+})
+
+app.put('/orders/:order_id', (req, res) => {})
+
+//To get orders from user
+app.get('/orders/:user_id', (req, res) => {
+    //
+})
+
+//Port forwarding
+app.listen(port, () => {
+  console.log(`SendIT Server running on port ${port}`)
+})
