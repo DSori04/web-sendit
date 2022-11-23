098633701ddcac01f3b38ef765b5a97882bb159c
Albert Bru <68111494+A1bert04@users.noreply.github.com>
2022-11-23 13:21:25 +0100

    Title fixed responsive New Order

--- src/NewOrder/NewOrder.jsx
+++ src/NewOrder/NewOrder.jsx
@@ -19,7 +19,7 @@ export function NewOrder() {
         const origin = Object.fromEntries(new FormData(e.target));
         console.log(origin);
         setstep(2)
-        
+
     }
     const handleSubmitDestination = (e) => {
         e.preventDefault();
@@ -34,23 +34,28 @@ export function NewOrder() {
     }
     return (
         <>
-        <Helmet>
-            <title>New Order</title>
-            <meta name="description" content="New Order" />
-        </Helmet>
+            <Helmet>
+                <title>New Order</title>
+                <meta name="description" content="New Order" />
+            </Helmet>
             <AppContextProvider>
                 <Navbar />
             </AppContextProvider>
             <div className="flex flex-row w-full justify-center min-h-[90vh]">
                 <div className="xl:w-3/4 lg:absolute lg:pt-0 pt-16 top-16 sm:bottom-14 bottom-28 w-full flex flex-col h-max lg:px-20 px-6 font-main">
-                    <h1 className=" font-bold text-purple1 text-5xl lg:pt-14 pt-8 select-none">Nuevo pedido</h1>
+                    <div>
+                        <h1 className=" font-bold text-5xl lg:pt-14 pt-8 select-none lg:text-left text-center">
+                            <span className="text-purple1">Nuevo </span> 
+                            <span className=" text-black">pedido</span>
+                        </h1>
+                    </div>
                     {step == 1 && <div className="w-full h-max flex lg:flex-row flex-col justify-center">
-                        <div className="flex flex-row max-h-fit justify-center lg:py-20 py-10 lg:pr-10 min-w-max ">
+                        <div className="flex flex-row max-h-fit justify-center lg:py-20 py-10 pb-2 lg:pr-10 min-w-max ">
                             <div id="steps" className="flex lg:flex-col flex-row w-48 justify-around">
                                 <div className="w-full font-main text-sm flex lg:flex-row flex-col-reverse justify-end">
                                     <div className="flex flex-col justify-center">
                                         <span className="inline-block lg:text-right text-center font-bold text-xl h-max lg:mr-3 lg:mt-0 mt-3">
-                                            Datos Envio
+                                            Datos Orígen
                                         </span>
                                     </div>
                                     <div className="flex lg:w-max w-24 lg:justify-end justify-center">
@@ -85,35 +90,40 @@ export function NewOrder() {
                                 </div>
                             </div>
                         </div>
-                        <div id="originform" className="lg:block flex-row justify-center min-w-full">
-                            <form onSubmit={(e) => handleSubmitOrigin(e)} className="lg:block inline-block lg:w-auto w-64 mr-0">
-                                <label htmlFor="name" className="text-main block mt-8">Nombre completo <span className="text-main text-red1">*</span></label>
-                                <input type="text" name="name" id="origin_name" className="border-b-2 inline-block" required />
+                        <div className="lg:w-auto w-full flex flex-row justify-center">
+                            <div id="originform" className="lg:block flex-row justify-center min-w-min">
+                                <form onSubmit={(e) => handleSubmitOrigin(e)} className="lg:block flex flex-col lg:w-full w-64">
+                                    <label htmlFor="name" className="text-main block mt-8">Nombre completo <span className="text-main text-red1">*</span></label>
+                                    <input type="text" name="name" id="origin_name" className="border-b-2 inline-block" required />
 
-                                <label htmlFor="origin_email" className="text-main block mt-8">Correo Electrónico <span className="text-main text-red1">*</span></label>
-                                <input type="email" name="email" id="origin_email" className="border-b-2 inline-block" required></input>
+                                    <label htmlFor="origin_email" className="text-main block mt-8">Correo Electrónico <span className="text-main text-red1">*</span></label>
+                                    <input type="email" name="email" id="origin_email" className="border-b-2 inline-block" required></input>
 
-                                <label htmlFor="origin_tlf" className="text-main block mt-8">Teléfono  <span className="text-main text-red1">*</span></label>
-                                <input type="tel" name="tlf" id="origin_tlf" className="border-b-2 inline-block" required />
+                                    <label htmlFor="origin_tlf" className="text-main block mt-8">Teléfono  <span className="text-main text-red1">*</span></label>
+                                    <input type="tel" name="tlf" id="origin_tlf" className="border-b-2 inline-block" required />
 
-                                <label htmlFor="origin_addr1name" className="text-main block mt-8">Dirección 1 <span className="text-main text-red1">*</span></label>
-                                <input type="text" name="addr1" id="origin_addr1" className="border-b-2 inline-block" required />
+                                    <label htmlFor="origin_addr1name" className="text-main block mt-8">Dirección 1 <span className="text-main text-red1">*</span></label>
+                                    <input type="text" name="addr1" id="origin_addr1" className="border-b-2 inline-block" required />
 
-                                <label htmlFor="origin_addr_2" className="text-main block mt-8">Dirección 2</label>
-                                <input type="text" name="addr2" id="origin_addr2" className="border-b-2 inline-block" />
+                                    <label htmlFor="origin_addr_2" className="text-main block mt-8">Dirección 2</label>
+                                    <input type="text" name="addr2" id="origin_addr2" className="border-b-2 inline-block" />
 
-                                <div className="w-full flex flex-row">
-                                    <div className="w-1/3">
-                                        <label htmlFor="city" className="text-main block mt-8">Ciudad <span className="text-main text-red1">*</span></label>
-                                        <input type="text" name="city" id="origin_city" className="border-b-2 inline-block w-3/4" required />
+                                    <div className="w-full flex flex-row">
+                                        <div className="w-1/3">
+                                            <label htmlFor="city" className="text-main block mt-8">Ciudad <span className="text-main text-red1">*</span></label>
+                                            <input type="text" name="city" id="origin_city" className="border-b-2 inline-block w-3/4" required />
+                                        </div>
+                                        <div className="w-1/4">
+                                            <label htmlFor="origin_cp" className="text-main block mt-8">CP <span className="text-main text-red1">*</span></label>
+                                            <input type="text" name="cp" id="origin_cp" className="border-b-2 inline-block w-2/3" required />
+                                        </div>
                                     </div>
-                                    <div className="w-1/4">
-                                        <label htmlFor="origin_cp" className="text-main block mt-8">CP <span className="text-main text-red1">*</span></label>
-                                        <input type="text" name="cp" id="origin_cp" className="border-b-2 inline-block w-2/3" required />
+                                    <div className="flex flex-row lg:justify-start justify-center">
+                                        <input type="submit" value="Continuar" className="block mt-8 bg-purple1 font-main text-white px-4 py-1 rounded-full font-semibold drop-shadow-xl max-w-fit lg:hover:hue-rotate-15" />
+
                                     </div>
-                                </div>
-                                <input type="submit" value="Continuar" className="block mt-8 bg-purple1 font-main text-white px-4 py-1 rounded-full font-semibold drop-shadow-xl lg:hover:hue-rotate-15" />
-                            </form>
+                                </form>
+                            </div>
                         </div>
                         <div id="image" className="sm:block hidden min-h-fit">
                             <div className="flex flex-col h-full justify-center">
@@ -259,6 +269,7 @@ export function NewOrder() {
                                 </div>
                                 <button className="w-40 mt-12 bg-purple1 h-12 rounded-full">
                                     <img src={payicon} className="inline-block h-7"></img>
+
                                     <span className="text-white text-xl font-semibold ml-3 inline-block">Pagar</span>
 
                                 </button>
